73b5830a019497600b736de6c9237266c030eea6
ruinhadan <ihateyoutheory@live.com>
2021-06-04 22:18:22 +0530

    Added more styling

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,11 +1,13 @@
-mapboxgl.accessToken = mapToken;
-const lnglat = Array.from(coordinates.split(',')).map(x => parseFloat(x))
-const map = new mapboxgl.Map({
+mapboxgl.accessToken=mapToken;
+const lnglat=Array.from(coordinates.split(',')).map(x => parseFloat(x))
+const map=new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: lnglat,
         zoom: 10
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
         .setLngLat(lnglat)
--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-const Campground = require('../models/campground');
-const cities = require('./cities')
-const { descriptors, places } = require('./seedHelpers')
+const mongoose=require('mongoose');
+const Campground=require('../models/campground');
+const cities=require('./cities')
+const { descriptors, places }=require('./seedHelpers')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
         useNewUrlParser: true,
@@ -9,20 +9,20 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
         useCreateIndex: true
 });
 
-const db = mongoose.connection;
+const db=mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
         console.log("Database connected");
 });
 
-const randomSampler = array => array[Math.floor(Math.random() * array.length)];
+const randomSampler=array => array[Math.floor(Math.random()*array.length)];
 
-const seedDatabase = async () => {
+const seedDatabase=async () => {
         await Campground.deleteMany({});
-        for (let i = 0; i < 50; i++) {
-                const city = randomSampler(cities);
-                const price = Math.floor(Math.random() * 30) + 20;
-                const campground = new Campground({
+        for (let i=0; i<50; i++) {
+                const city=randomSampler(cities);
+                const price=Math.floor(Math.random()*30)+20;
+                const campground=new Campground({
                         author: '60b38ffd4617d33090789986',
                         location: `${city.city}, ${city.state}`,
                         title: `${randomSampler(descriptors)} ${randomSampler(places)}`,
